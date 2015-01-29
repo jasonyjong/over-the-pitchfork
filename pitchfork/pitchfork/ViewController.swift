389b4e446c91a9c4f7259e78b73dc9641f6d0bf8
Jason Jong <jjong@stanford.edu>
2015-01-29 15:58:45 -0500

    Init commit for ios

---
+++ pitchfork/pitchfork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchfork
+//
+//  Created by Jason YeeHarn Jong on 1/29/15.
+//  Copyright (c) 2015 Jason YeeHarn Jong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
